684749118f821d5f3c3ae211c0d41fc43f7f00ec
LogLand <contact@logland.life>
2025-03-03 02:20:50 +0000

    fixed?

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+
+import tailwind from '@astrojs/tailwind';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  // site: 'https://logland.life',
+  site: 'https://logland.github.io',
+  output: 'static',
+  integrations: [mdx(), tailwind(), sitemap()],
+  vite: {
+      build: {
+        assetsInlineLimit: 1
+    }
+  }
+});
